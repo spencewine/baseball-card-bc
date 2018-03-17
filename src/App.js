55cbf6ae330985a83ebd8de7ad2f7acd45a4be94
Ryan Driscoll <ryan@Ryans-MacBook-Pro.local>
2018-03-17 13:47:27 -0500

    fixed starter pack with real player data

--- src/App.js
+++ src/App.js
@@ -4,30 +4,34 @@ import './App.css';
 import axios from 'axios';
 import secrets from './secrets'
 
-import {Block, Blockchain} from './blockchain.js'
+import { Block, Blockchain } from './blockchain.js'
 
 class App extends Component {
 
-    constructor (props) {
-      super (props)
-      this.state = {blockchain: new Blockchain()}
-      this.addUserToChain = this.addUserToChain.bind(this);
-    }
+  constructor(props) {
+    super(props)
+    this.state = { blockchain: new Blockchain() }
+    this.addUserToChain = this.addUserToChain.bind(this);
+  }
 
-    async componentDidMount(){
+  async componentDidMount() {
 
-        //gets player data for 2017 batting title leaders
-        let data = await axios.get(secrets.sportradarStats)
-        let mlbBattingLeaders = data.data.leagues[0].hitting.batting_average.players
-        mlbBattingLeaders.map((player) => {
-            const playerURL = secrets.playerProfile(player.id)
-            return axios.get(playerURL)
-        })
+    //gets player data for 2017 batting title leaders
+    let data = await axios.get(secrets.sportradarStats)
+    let mlbBattingLeaders = data.data.leagues[0].hitting.batting_average.players
+    // mlbBattingLeaders.map((player) => {
+    //   const playerURL = secrets.playerProfile(player.id)
+    //   return axios.get(playerURL)
+    // })
 
-        const PlayerDATA = await Promise.all(mlbBattingLeaders)
-    }
+    // const PlayerDATA = await Promise.all(mlbBattingLeaders)
+    console.log('Playerdata', JSON.stringify(mlbBattingLeaders, null, 2))
+    this.setState({
+      playerData: mlbBattingLeaders
+    })
+  }
 
-  addUserToChain (dataObj) {
+  addUserToChain(dataObj) {
     const timeStamp = new Date();
     const newBlock = new Block(timeStamp, dataObj)
     this.state.blockchain.addBlock(newBlock);
@@ -38,7 +42,11 @@ class App extends Component {
     console.log(this.state.blockchain)
     return (
       <div className="App">
-        <Createuser addUserToChain={this.addUserToChain} blockchain={this.state.blockchain} />
+        <Createuser
+          addUserToChain={this.addUserToChain}
+          blockchain={this.state.blockchain}
+          playerData={this.state.playerData}
+        />
       </div>
     );
   }
