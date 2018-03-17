dceb36810670d5455ba8a7fb3703d1519f4d6b93
spencewine <sdgoodwine@gmail.com>
2018-03-17 15:25:48 -0500

    basic trading

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 export default class Card extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            selected: false,
+        }
+
+        this.toggleTradeCard = this.toggleTradeCard.bind(this)
+    }
+
+    componentWillUpdate(nextProps, nextState){
+        console.log("NEXT STATE", nextState)
+
+    }
+
+    toggleTradeCard(){
+        this.setState({
+            selected: !this.state.selected
+        }, () => {
+            this.state.selected ?
+                this.props.addToTradeArray(this.props.card) :
+                null;
+
+        })
+
+    }
+
+
  render() {
+     const selectedStyle = {
+         borderColor: 'green'
+     }
 
   return (
-   <div className="card-container">
+   <div style={this.state.selected ? selectedStyle : {}} className="card-container" onClick={this.toggleTradeCard}>
         <img className="player-image" src={this.props.card.photo} />
         <h3>{this.props.card.firstName}</h3>
         <h3>{this.props.card.lastName}</h3>
--- src/UserPanel.js
+++ src/UserPanel.js
@@ -4,17 +4,32 @@ import Card from './Card.js';
 
 
 export default class User extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            tradeArray: []
+        }
+    }
+
+    addToTradeArray(card){
+        const nextTradeArray = this.state.tradeArray.slice()
+        this.setState({
+            tradeArray: [...nextTradeArray, card]
+        }, () => console.log("TRADE ARRAY", this.state.tradeArray))
+
+    }
 
     render () {
-
+        const { user, selectCards } = this.props
         return (
             <div className="one-user">
-                <h1>{this.props.user.name}</h1>
-                { this.props.user.cards.map(card => {
-                    return <Card key={card.id} card={card} />
-                }) 
+                <h1>{user.name}</h1>
+                { user.cards.map(card => {
+                    return <Card key={card.id} card={card} user={user.name} selectCards={selectCards} addToTradeArray={this.addToTradeArray.bind(this)}/>
+                })
                 }
             </div>
         )
-    }   
+    }
 }
